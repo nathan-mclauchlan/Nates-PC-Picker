f6630b72656c6b42f35a4568aeb5aec04f6673fe
Dainyleen Masangkay <dainyleen@gmail.com>
2021-04-13 10:29:36 +1200

    added useEffect

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,12 @@
-import React { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App () {
   const [widgets] = useState([])
+
+  useEffect(() => {
+    console.log('using the effect')
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
