b240d1d0eaa01eb1c73db90d0c1a20c4b19a551a
Caleb Ion <calebionpersonal@gmail.com>
2021-04-13 10:13:43 +1200

    Add react hooks

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function App () {
+function App() {
+  const [widgets, setWidgets] = useState([])
+
+  useEffect(() => {
+    console.log('using the effect')
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
