dd60feb83cf811463916f54c5a44ce090665b84a
Dainyleen Masangkay <dainyleen@gmail.com>
2021-04-13 11:16:33 +1200

    Showed the list of widgets on the browser

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from 'react'
 import { getWidgets } from '../api'
 
 function App () {
-  const [widgets] = useState([])
+  const [widgets, setWidgets] = useState([])
 
   useEffect(() => {
     // console.log('using the effect')
     getWidgets()
       .then(widgets => {
-        console.log(widgets)
+        setWidgets(widgets)
       })
   }, [])
 
   return (
     <div>
       <h1>Widgets for the win!</h1>
+        <ul>
+          {widgets.map((widget) => <li key={widget.id}>{widget.name}</li>)}
+        </ul>
     </div>
   )
 }
