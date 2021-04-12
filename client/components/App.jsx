f8406d1a1f2ff03025cd012e5100adf684d41159
Dainyleen Masangkay <dainyleen@gmail.com>
2021-04-13 10:49:00 +1200

    Added the getWidgets function and see the array from console

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import { getWidgets } from '../api'
 
 function App () {
   const [widgets] = useState([])
 
   useEffect(() => {
-    console.log('using the effect')
+    // console.log('using the effect')
+    getWidgets()
+      .then(widgets => {
+        console.log(widgets)
+      })
   }, [])
 
   return (
