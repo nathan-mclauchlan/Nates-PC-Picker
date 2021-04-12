448069dcf1ba0b885f6fbfb2c947b6d3692db847
sam-taylor165 <taylsj1908@gmail.com>
2021-04-13 10:56:31 +1200

    app component renders widgets

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getWidgets } from '../api'
 
-function App () {
+function App() {
+  const [widgets, setWidgets] = useState([])
+  //const [count, setCount] = useState(0)
+
+  // useEffect(getWidgets(), [])
+
+  useEffect(() => {
+    getWidgets()
+      .then(widgets => {
+        setWidgets(widgets)
+        return null
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
+      <ul>
+        {widgets.map((widget) => <li key={widget.id}>{widget.name}</li>)}
+      </ul>
     </div>
   )
 }
