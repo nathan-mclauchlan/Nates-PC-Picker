062244c91778e2d54a5401e157ddf84e2bb53d0c
nathan-mclauchlan <nathanmclauchlan21@gmail.com>
2021-04-13 10:48:27 +1200

    Can see widgets from useEffect function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react'
+import { getWidgets } from '../api'
 
 function App () {
   const [widgets,] = useState ([])
 
   useEffect(() => {
-    console.log('using the effect')
-  })
+    getWidgets()
+    .then(widgets => {
+      console.log(widgets)
+    })
+    // .catch(e = {
+    //   console.log(e)
+    // })
+    // console.log('using the effect')
+  }, [])
+
+
+  return (
+    <div>
+      <h1>Widgets for the win!</h1>
+    </div>
+  )
 }
 
 export default App
